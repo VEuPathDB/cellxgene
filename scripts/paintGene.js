7aa6604df293a08992e70153deac12f10ec84ee1
John Brestelli <jbrestell@gmail.com>
2022-07-15 10:28:56 -0400

    wait for elements before trying to modify them

--- scripts/paintGene.js
+++ scripts/paintGene.js
@@ -1,31 +1,51 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 window.addEventListener("load", function() {
-  console.log("In event listener");
-  const queryString = window.location.search;
+    const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
   const gene = urlParams.get("gene");
   const compact = urlParams.get("compact");
 
-  if(compact === "1") {
-    console.log("In compact");
-    var temp1 = document.getElementsByClassName("container")[0].childNodes[0];
-    temp1.style.gridTemplateColumns = '[left-sidebar-start] 0px [left-sidebar-end graph-start] auto [graph-end right-sidebar-start] 400px [right-sidebar-end]';
-    temp1.style.minWidth = "800px";
+ waitForElm('.bp3-button-group').then((elm) => {
 
-    Array.prototype.forEach.call(document.getElementsByClassName("bp3-button-group"), function(element) { console.log(element); element.style.visibility="hidden"; });
-    Array.prototype.forEach.call(document.getElementsByClassName("bp3-popover-wrapper"), function(element) { console.log(element); element.style.visibility="hidden"; });
-    Array.prototype.forEach.call(document.getElementsByClassName("bp3-heading"), function(element) { console.log(element); element.style.visibility="hidden"; });
-    Array.prototype.forEach.call(document.getElementsByClassName("bp3-intent-primary"), function(element) { console.log(element); element.style.visibility="hidden"; });
+  if(compact === "1") {
+    var temp1 = document.getElementsByClassName("container")[0].childNodes[0];
+
+      //or something more meaningful here
+      temp1.style.gridTemplateColumns = '[left-sidebar-start] 0px [left-sidebar-end graph-start] auto [graph-end right-sidebar-start] 400px [right-sidebar-end]';
+      temp1.style.minWidth = "800px";
+
+      Array.prototype.forEach.call(document.getElementsByClassName("bp3-button-group"), function(element) { element.style.visibility="hidden"; });
+      Array.prototype.forEach.call(document.getElementsByClassName("bp3-popover-wrapper"), function(element) { element.style.visibility="hidden"; });
+      Array.prototype.forEach.call(document.getElementsByClassName("bp3-heading"), function(element) {  element.style.visibility="hidden"; });
+      Array.prototype.forEach.call(document.getElementsByClassName("bp3-intent-primary"), function(element) {  element.style.visibility="hidden"; });
   }
   
   if(gene != null) {
-    console.log("Gene declared");
+
     var els = document.getElementsByClassName("bp3-button");
     Array.prototype.forEach.call(els, function(element) {
       var id = element.getAttribute("data-testid");
       if(id === "colorby-" + gene) {
-        console.log("Testid");
-        console.log(element);
         element.click();
       }
     });
@@ -33,11 +53,11 @@ window.addEventListener("load", function() {
     Array.prototype.forEach.call(els, function(element) {
       var id = element.getAttribute("data-testid");
       if(id === "maximize-" + gene) {
-        console.log("Maximise");
-        console.log(element);
+
         element.click();
       }
     });
 
   }
 });
+}, false);
